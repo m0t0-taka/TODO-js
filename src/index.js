052ba10cbcabaa7a2b50c8f54c1e208bdcdb2e75
m0t0-taka <81753585+m0t0-taka@users.noreply.github.com>
2021-11-11 10:56:25 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import "./styles.css";
 
 const onClickAdd = () => {
+  // フォームの入力値を取得とクリア
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
-  // const li = document.createElement("li");
+  createIncompleteList(inputText);
+};
 
+// 「未完了のTODO」からリストを削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+const createIncompleteList = (text) => {
+  // divの生成とclass名付与
   const div = document.createElement("div");
   div.className = "list-row";
 
+  // liの生成とliのtextを取得
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   // 完了buttonを生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  // 「完了」ボタンクリックで完了ボタンの親要素ごと未完了のTODOから削除
   completeButton.addEventListener("click", () => {
     deleteFromIncompleteList(completeButton.parentNode);
     // 完了リストに追加する要素
@@ -28,6 +39,13 @@ const onClickAdd = () => {
 
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
 
     addTarget.appendChild(li);
     addTarget.appendChild(backButton);
@@ -43,16 +61,11 @@ const onClickAdd = () => {
   });
 
   // HTMLの階層を生成
-  // ul.appendChild(div);
   div.appendChild(li);
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
 
   document.getElementById("incomplete-list").appendChild(div);
-};
-
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
